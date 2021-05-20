f7e32a8aa9cc23be2919da79d2a920cbd3f0d969
Vitaliy <vitaliy0187@gmail.com>
2021-05-20 21:43:36 +0300

    add header section

---
+++ gmail-clone/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <h2>Hello Gmail</h2>
+    </div>
+  );
+}
+
+export default App;
